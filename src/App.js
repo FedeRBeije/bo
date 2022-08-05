584837be6420a83a73b32fc34542ca33bfacad8c
FedeRBeije <federico.reghini@beije.it>
2022-08-05 13:04:04 +0200

    bo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+
+const instance = axios.create({
+  baseURL: "https://dev-mgmt.beije.it/",
+  headers: {
+    Authorization: "Bearer ",
+  },
+});
 
 function App() {
+  const [data, setData] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={async () => await instance("/admin/material", {
+      data,
+      method: "post"
+    })} className="App">
+      <input type="file" onChange={e => {
+        setData(e)
+      }} />
+    </form>
   );
 }
 
