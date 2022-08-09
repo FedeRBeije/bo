ae29846821d843b8267b4b4e58b112d5ed4b9d0b
FedeRBeije <federico.reghini@beije.it>
2022-08-09 12:59:16 +0200

    b

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import UploadFile from './UploadFile';
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/mgmt/",
+  baseURL: "https://dev-mgmt.beije.it/",
   headers: {
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBtYWlsLmNvbSIsInJvbGVzIjpbIlVTRVIiLCJBRE1JTiIsIkhSIiwiQ09NTUVSQ0lBTCJdLCJpYXQiOjE2NTk3Nzc4NTUsImV4cCI6MTY1OTc4MTQ1NX0.z-W8dyXov7c1OqHTz3CNyeMJGnBS_kMIaOfWMWGZJTk",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBtYWlsLmNvbSIsInJvbGVzIjpbIlVTRVIiLCJBRE1JTiIsIkhSIiwiQ09NTUVSQ0lBTCJdLCJpYXQiOjE2NjAwMzc1MjAsImV4cCI6MTY2MDA0MTEyMH0.UWoJs34imz_z0l7cyptpoNWNi9_RBZN3tSlm4Bs0Ny4",
   },
 });
 
@@ -46,6 +46,13 @@ async function link(resource, obj, method) {
   })
 }
 
+async function download(resource, obj, method) {
+  return await instance(resource, {
+    data: obj,
+    method
+  })
+}
+
 const App = ({ isNew }) => {
 
   const [state, setState] = useState(initState);
@@ -70,12 +77,12 @@ const App = ({ isNew }) => {
 
     saved = true;
 
-    if (state.link) link(`/admin/material_link/${id}`,{ ...state, file: null, fileName: null }, "post");
+    if (state.link) link(`/admin/material_link/${id = 0}`,{ ...state, file: null, fileName: null }, "post");
 
     else {
       const res = await uploadDoc("/admin/material", formData, "post");
       console.log(res);
-      // link(`/admin/material_link/${id}`,{ ...state, link: null, fileName: null }, "post");
+      link(`/admin/material_link/${res.id}`,{ ...state, link: null, fileName: null }, "post");
     }
 
   }
@@ -87,71 +94,74 @@ const App = ({ isNew }) => {
   return (
 
 
-    <form>
-
-      <CardContainerMemo head="Documento">
-
-        <Select
-          style={{ maxWidth: "50%", marginTop: "2rem" }}
-          value={state.academy}
-          label="Academy"
-          options={[
-            { value: "FE", label: "Front-end" },
-            { value: "BE", label: "Back-end" },
-          ]}
-          onChange={handleSetAcademy}
-        />
-
-        <Input
-          style={{ width: "50%", marginTop: "3rem" }}
-          placeholder="Titolo"
-          name="title"
-          value={state.title}
-          onChange={(e) => {
-            setState((p) => ({ ...p, title: e.target.value }));
-          }}
-        />
-
-        <Input
-          disabled={state.file_base64 ? true : false}
-          style={{ width: "50%", marginTop: "3rem" }}
-          placeholder="Link alla risorsa"
-          name="name"
-          value={state.link}
-          onChange={(e) => {
-            setState((p) => ({ ...p, link: e.target.value }));
-          }}
-        />
-
-        <UploadFile
-          disabled={state.link ? true : false}
-          style={{ marginTop: "3rem", marginBottom: "3rem" }}
-          value={state.file_base64}
-          onChange={e => {
-            setState({ ...state, file: e.content, fileName: e.fileName })
-          }}
-        />
-
-        <p>{state.fileName}</p>
-      </CardContainerMemo>
-
-      <CardContainerMemo
-        head="Descrizione"
-        style={{ minHeight: "30rem" }}
-      >
-        <textarea
-          style={{ width: "50%", height: "30rem", marginTop: "2rem" }}
-          placeholder="Descrizione"
-          name="title"
-          value={state.description}
-          onChange={(e) => {
-            setState((p) => ({ ...p, description: e.target.value }));
-          }}
-        />
-      </CardContainerMemo>
-
-      <SaveContainerMemo isNew={isNew} onSubmit={handleSubmitDoc} />
-    </form>
+   <>
+     <form>
+    
+       <CardContainerMemo head="Documento">
+    
+         <Select
+           style={{ maxWidth: "50%", marginTop: "2rem" }}
+           value={state.academy}
+           label="Academy"
+           options={[
+             { value: "FE", label: "Front-end" },
+             { value: "BE", label: "Back-end" },
+           ]}
+           onChange={handleSetAcademy}
+         />
+    
+         <Input
+           style={{ width: "50%", marginTop: "3rem" }}
+           placeholder="Titolo"
+           name="title"
+           value={state.title}
+           onChange={(e) => {
+             setState((p) => ({ ...p, title: e.target.value }));
+           }}
+         />
+    
+         <Input
+           disabled={state.file ? true : false}
+           style={{ width: "50%", marginTop: "3rem" }}
+           placeholder="Link alla risorsa"
+           name="name"
+           value={state.link}
+           onChange={(e) => {
+             setState((p) => ({ ...p, link: e.target.value }));
+           }}
+         />
+    
+         <UploadFile
+           disabled={state.link ? true : false}
+           style={{ marginTop: "3rem", marginBottom: "3rem" }}
+           value={state.file}
+           onChange={e => {
+             setState({ ...state, file: e.content, fileName: e.fileName })
+           }}
+         />
+    
+         <p>{state.fileName}</p>
+       </CardContainerMemo>
+    
+       <CardContainerMemo
+         head="Descrizione"
+         style={{ minHeight: "30rem" }}
+       >
+         <textarea
+           style={{ width: "50%", height: "30rem", marginTop: "2rem" }}
+           placeholder="Descrizione"
+           name="title"
+           value={state.description}
+           onChange={(e) => {
+             setState((p) => ({ ...p, description: e.target.value }));
+           }}
+         />
+       </CardContainerMemo>
+    
+       <SaveContainerMemo isNew={isNew} onSubmit={handleSubmitDoc} />
+     </form>
+    
+   </>
   )
 }
 
